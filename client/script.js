d29ea9d771125aa229e38b8fa37641d3fada5b8b
ndw1117 <ndw1117@g.rit.edu>
2024-11-16 14:36:39 -0500

    Input Validation And Status Message

--- client/script.js
+++ client/script.js
@@ -30,12 +30,19 @@ const init = () => {
     // Store reference to the submit button
     const submitButton = document.getElementById('submit');
 
+    // Store reference to the status message
+    const statusMessage = document.getElementById('statusMessage');
+
+    // Make sure status message isn't showing by default
+    statusMessage.style.display = 'none';
+
+
     // Set event handlers for each creature image option
-    for (let i =0; i < imagesSet1.length; i++) {
+    for (let i = 0; i < imagesSet1.length; i++) {
         imagesSet1[i].onclick = () => {
 
             // Reset any existing selection
-            for(let img of imagesSet1) {
+            for (let img of imagesSet1) {
                 img.style.boxShadow = 'none';
             }
 
@@ -43,17 +50,17 @@ const init = () => {
             imagesSet1[i].style.boxShadow = '0px 0px 20px 4px white';
 
             // Store this as the currently selected option
-            console.log(`You selected creature ${i+1}`);
+            console.log(`You selected creature ${i + 1}`);
             selectedCreature = i + 1;
         }
     }
 
     // Set event handlers for each power-up image option
-    for (let i =0; i < imagesSet2.length; i++) {
+    for (let i = 0; i < imagesSet2.length; i++) {
         imagesSet2[i].onclick = () => {
 
             // Reset any existing selection
-            for(let img of imagesSet2) {
+            for (let img of imagesSet2) {
                 img.style.boxShadow = 'none';
             }
 
@@ -61,7 +68,7 @@ const init = () => {
             imagesSet2[i].style.boxShadow = '0px 0px 20px 4px white';
 
             // Store this as the currently selected option
-            console.log(`You selected power-up ${i+1}`);
+            console.log(`You selected power-up ${i + 1}`);
             selectedPowerUp = i + 1;
         }
     }
@@ -70,35 +77,58 @@ const init = () => {
     // Set event handler for submission
     submitButton.onclick = async () => {
 
-        // Build the data object and send it to the backend using the /submit endpoint
-
-        let dataObject = {
-            lonelyInput: lonelyInput.value,
-            creature: selectedCreature,
-            positiveInput: positiveInput.value,
-            powerUp: selectedPowerUp
+        if (lonelyInput.value === "" || positiveInput.value === "" || selectedCreature === 0 || selectedPowerUp === 0) {
+            statusMessage.innerHTML = 'Please make sure all 4 questions have been answered.';
+            statusMessage.style.color = 'rgb(240, 88, 88)';
+            statusMessage.style.display = 'block';
         }
-
-        try {
-            const response = await fetch('/submit', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'  
-                },
-                body: JSON.stringify(dataObject)
-            });
-
-            if (response.ok) {
-                const jsonResponse = await response.json();
-                console.log('Data submitted successfully:', jsonResponse);
-            } else {
-                console.error('Error submitting data:', response.statusText);
+        else {
+            // Build the data object and send it to the backend using the /submit endpoint
+            let dataObject = {
+                lonelyInput: lonelyInput.value,
+                creature: selectedCreature,
+                positiveInput: positiveInput.value,
+                powerUp: selectedPowerUp
             }
-        }
-        catch (error) {
-            console.error('Error making POST request:', error);
-        }
-    };
+
+            // Tries to use the server's submit endpoint to transfer the user input data to the backend data structure
+            try {
+                const response = await fetch('/submit', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dataObject)
+                });
+
+                if (response.ok) {
+                    const jsonResponse = await response.json();
+                    console.log('Data submitted successfully:', jsonResponse);
+
+                    // Display success message to the user
+                    statusMessage.innerHTML = 'Thank you for your input. Now connect with others using our game.';
+                    statusMessage.style.color = 'greenyellow';
+                    statusMessage.style.display = 'block';
+
+                } else {
+                    console.error('Error submitting data:', response.statusText);
+
+                    // Display error message to the user
+                    statusMessage.innerHTML = 'Error submitting your data. Please contact project developers.';
+                    statusMessage.style.color = 'rgb(240, 88, 88)';
+                    statusMessage.style.display = 'block';
+                }
+            }
+            catch (error) {
+                console.error('Error making POST request:', error);
+
+                // Display error message to the user
+                statusMessage.innerHTML = 'Error submitting your data. Please contact project developers.';
+                statusMessage.style.color = 'rgb(240, 88, 88)';
+                statusMessage.style.display = 'block';
+            }
+        };
+    }
 
 };
 
